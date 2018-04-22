fdbd4c93d0b4e28edf73cbcf3a6f2447397267d9
Brandon Rothweiler <brandonrothweiler@gmail.com>
2018-04-22 19:12:13 -0400

    Change CSRF middleware to replace res.send instead of res.render

--- csrf.js
+++ csrf.js
@@ -3,29 +3,27 @@ const config = require("./config/config");
 const bcrypt = require("bcrypt-nodejs");
 
 let csrfMiddleware = function (req, res, next) {
-    let originalRenderFunction = res.render;
+    let originalSendFunction = res.send;
 
-    //Override the render function with a new function that adds a CSRF token to all forms
-    res.render = function (view, locals, callback) {
-        originalRenderFunction.call(res, view, locals, function(err, html){
-            html = csrfProtectHTML(html, req.sessionID);
-            
-            if (typeof callback === "function"){
-                callback(err, html);
-            } else {
-                res.send(html);
-            }
-        });
+    //Replace the res.send function with one that adds a CSRF token to all HTML forms
+    res.send = function (body) {
+        if (typeof body === "string"){
+            originalSendFunction.call(res, csrfProtectHTML(body, req.sessionID));
+        } else if (Buffer.isBuffer(body) && res.get("Content-Type") === "text/html"){
+            originalSendFunction.call(res, csrfProtectHTML(body.toString(), req.sessionID));
+        } else {
+            originalSendFunction.call(res, body);
+        }
     };
 
     //For POST requests, validate the token
-    if (req.method === "POST"){
-        if (!req.body["CSRFtoken"]){
+    if (req.method === "POST") {
+        if (!req.body["CSRFtoken"]) {
             return res.status(400).send("CSRFtoken not present");
         }
 
         let result = bcrypt.compareSync(req.sessionID + config.CSRF_SECRET, req.body["CSRFtoken"]);
-        if (!result){
+        if (!result) {
             return res.status(400).send("CSRFtoken not valid");
         }
     }
@@ -34,7 +32,7 @@ let csrfMiddleware = function (req, res, next) {
 };
 
 //Modify html to add a CSRF token to all forms
-function csrfProtectHTML(html, sessionID){
+function csrfProtectHTML(html, sessionID) {
     let $ = cheerio.load(html);
 
     //Create hidden HTML form field
